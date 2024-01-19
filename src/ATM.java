93a81860b54417d25b47e1806c2033d12988c76a
BT_1N3_26 <sample@bths.edu>
2024-01-19 08:54:19 -0500

    FINAL

--- src/ATM.java
+++ src/ATM.java
@@ -26,6 +26,7 @@ public class ATM {
         customerName = name;
         System.out.print("Create a new pin for your account: ");
         customerPin = scan.nextInt();
+        ConsoleUtility.clearScreen();
         customer = new Customer(customerName, customerPin);
         System.out.println("Re-enter the pin of your new account!");
         int repeat = -1;
@@ -39,17 +40,17 @@ public class ATM {
                 System.out.println(" ");
             }
         }
+        ConsoleUtility.clearScreen();
         mainMenu();
     }
 
 
     public void mainMenu()  {
-        System.out.println("");
         System.out.print("Welcome to the ATM! ");
         while (end != 7) {
             System.out.println("\n1. Withdraw Money\n2. Deposit Money\n3. Transfer Money Between Accounts\n4. Get Account Balances\n5. Get Transaction History\n6. Change PIN\n7. Exit");
 
-            int option = 9;
+            int option = 8;
             while (option > 7) {
                 System.out.print("Enter a valid number: ");
                 option = scan.nextInt();
@@ -58,34 +59,71 @@ public class ATM {
                     System.out.println("Number is not valid try again! ");
                 }
             }
+            ConsoleUtility.clearScreen();
             if (option == 1) {
                 customer.withDraw();
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
                 reEnterPin();
             }
 
             if (option == 2) {
                 customer.deposit();
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
                 reEnterPin();
             }
 
             if (option == 3) {
                 customer.transferMoney();
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
                 reEnterPin();
             }
 
             if (option == 4) {
                 System.out.println(customer.getBalances());
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
             }
 
             if (option == 5) {
                 System.out.println(customer.getTransactionHistory());
                 System.out.println(customer.getSReceipt("Checked Transaction History!"));
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
                 reEnterPin();
             }
 
             if (option == 6) {
                 changePin();
                 System.out.println(customer.getSReceipt("Changed Pin!"));
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    System.out.println("error");
+                }
+                ConsoleUtility.clearScreen();
                 reEnterPin();
             }
 
@@ -115,6 +153,7 @@ public class ATM {
         }
         else {
             System.out.println("Thank you for using the ATM and have a great day! ");
+            end = 7;
         }
     }
 
